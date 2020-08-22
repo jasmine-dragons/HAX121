4640308ccaa1a9a536077886ec155bc4979a5b3f
aidenszeto <aszeto35@gmail.com>
2020-08-22 14:31:52 -0700

    18

---
+++ problem18.py
@@ -0,0 +1,31 @@
+# HAX 121 2020 Python (3.6.0) Submission Template
+# Please fill out the info fields below for identification
+# Do not modify any part of the template
+
+# Team Name:
+# Team Number:
+# Problem Number:
+
+import array
+import math
+import re
+import string
+
+# Class and function definitions:
+def biggest():
+    input_string = input('input: ')
+    split = input_string.split(' ')
+    digits = int(split[0])
+    sum = int(split[1])
+    max = 0
+    count = 0
+    for i in range(10 ** (digits)):
+        digit_sum = 0
+        for dig in str(i):
+            digit_sum += int(dig)
+        if digit_sum == sum:
+            max = i
+    print(max)
+
+# Main program:
+biggest()
