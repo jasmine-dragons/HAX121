c23fc9227c43884f2f7aeba8f97fb06d67971f7d
nishantbalaji <44332326+NishantBalaji@users.noreply.github.com>
2020-08-22 15:18:00 -0700

    15

---
+++ problem15.py
@@ -0,0 +1,35 @@
+# HAX 121 2020 Python (3.6.0) Submission Template
+# Please fill out the info fields below for identification
+# Do not modify any part of the template
+
+# Team Name:
+# Team Number:
+# Problem Number:
+
+import array
+import math
+import re
+import string
+
+# Class and function definitions:
+def goodNum():
+    num = input("num: ")
+    num = str(num)
+    length = len(num)
+
+    output = " "
+    length1 = length
+    for i in range(length):
+        if num[i] != '0':
+            value = num[i]
+            for j in reversed(range(length1-1)):
+                value += "0"
+            length1-=1
+            output += f"{value} "
+
+
+    print(output)
+
+# Main program:
+
+goodNum()
