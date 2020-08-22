86e3909114251c82b251f2bf5893eb7c725609e3
Kenzie Tee <62974348+teekenzie@users.noreply.github.com>
2020-08-22 12:18:28 -0700

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,19 @@
+# HAX 121 2020 Python (3.6.0) Submission Template
+# Please fill out the info fields below for identification
+# Do not modify any part of the template
+
+# Team Name: LeadTech
+# Team Number: 
+# Problem Number: 7 
+
+import array
+import math
+import re
+import string
+
+# Class and function definitions:
+
+# Main program:
+
+def asscendSort(nums):
+    return sorted(nums, reverse=True)
