76599586386e25f3c24c3083e5df5cd86eb9f1ab
Aroop Biswal <aroopbiswal@gmail.com>
2020-08-22 13:25:19 -0700

    update

---
+++ problem10.py
@@ -0,0 +1,21 @@
+# HAX 121 2020 Python (3.6.0) Submission Template
+# Please fill out the info fields below for identification
+# Do not modify any part of the template
+
+# Team Name:LeadTech
+# Team Number:P074
+# Problem Number: 10
+
+import array
+import math
+import re
+import string
+
+# Class and function definitions:
+def removeExtraLetters(word):
+    
+
+# Main program:
+def main():
+    word = input("Enter Word: ").split()
+    word = string(word)
