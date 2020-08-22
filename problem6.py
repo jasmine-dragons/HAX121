4cb187cfc57cea1edf84ba85072d72fcac068886
Aroop Biswal <aroopbiswal@gmail.com>
2020-08-22 12:47:02 -0700

    Create problem6.py
    prob 6

---
+++ problem6.py
@@ -0,0 +1,29 @@
+# HAX 121 2020 Python (3.6.0) Submission Template
+# Please fill out the info fields below for identification
+# Do not modify any part of the template
+
+# Team Name:
+# Team Number:
+# Problem Number: 6
+
+import array
+import math
+import re
+import string
+
+# Class and function definitions:
+def getArea(nums):
+    nums = list(nums)
+    return .5 * nums[0] * nums[1]
+
+def checkForTriple(nums):
+    nums = list(nums)
+    a = nums[0]
+    b = nums[1]
+    c = nums[2]
+    if not a*a + b*b == c*c:
+        return 0
+    else:
+        return getArea(nums)
+
+# Main program:
