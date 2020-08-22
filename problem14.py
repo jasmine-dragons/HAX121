1a2f6287ba822122666d79fa4fcc43db81d3b8a7
nishantbalaji <44332326+NishantBalaji@users.noreply.github.com>
2020-08-22 14:00:47 -0700

    problem14

---
+++ problem14.py
@@ -0,0 +1,24 @@
+# HAX 121 2020 Python (3.6.0) Submission Template
+# Please fill out the info fields below for identification
+# Do not modify any part of the template
+
+# Team Name:
+# Team Number:
+# Problem Number:
+
+import array
+import math
+import re
+import string
+
+# Class and function definitions:
+def sortStrings():
+    list1 = input("input the first list, separated by a space between each value: ").split()
+    list2 = input("input the second list, separated by a space between each value: ").split()
+
+    comblist = list1 + list2
+    comblist = map(int, comblist)
+    print(sorted(comblist))
+
+# Main program:
+sortStrings()
