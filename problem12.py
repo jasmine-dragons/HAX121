0c971614a14d224c7d361d3495c70564660ab5c4
nishantbalaji <44332326+NishantBalaji@users.noreply.github.com>
2020-08-22 13:45:00 -0700

    problem12

---
+++ problem12.py
@@ -0,0 +1,25 @@
+# HAX 121 2020 Python (3.6.0) Submission Template
+# Please fill out the info fields below for identification
+# Do not modify any part of the template
+
+# Team Name:
+# Team Number:
+# Problem Number:
+
+import array
+import math
+import re
+import string
+
+# Class and function definitions:
+def steps():
+    distance = input("distance: ")
+    distance = int(distance)
+    fives = int(distance / 5)
+    distance = distance % 5
+    threes = int(distance / 3)
+    distance = distance % 3
+
+    print(int(fives + threes + distance))
+# Main program:
+steps()
