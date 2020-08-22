c33e5340aca3539a45c6f6c19f13ff258dddc466
aidenszeto <aszeto35@gmail.com>
2020-08-22 15:52:11 -0700

    10

--- problem10.py
+++ problem10.py
@@ -1,3 +1,10 @@
+# HAX 121 2020 Python (3.6.0) Submission Template
+# Please fill out the info fields below for identification
+# Do not modify any part of the template
+
+# Team Name: LeadTech
+# Team Number: P074
+# Problem Number: 10
 import array
 import math
 import re
